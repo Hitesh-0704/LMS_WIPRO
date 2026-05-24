b113987c5f4adfd4b1fc08bf3094d4db9168a1f2
Hitesh-0704 <hiteshthakare0704@gmail.com>
2026-05-24 23:09:39 +0530

    fix: use env variable for API base URL

--- learnhub/src/api/axios.js
+++ learnhub/src/api/axios.js
@@ -1,38 +1,9 @@
-//import axios from 'axios'
-//
-//const api = axios.create({
-//  baseURL: 'http://localhost:9797',
-//  headers: { 'Content-Type': 'application/json' }
-//})
-//
-//// Attach JWT token to every request
-//api.interceptors.request.use(config => {
-//  const token = localStorage.getItem('token')
-//  if (token) config.headers.Authorization = `Bearer ${token}`
-//  return config
-//})
-//
-//// Auto logout when server returns 401
-//api.interceptors.response.use(
-//  response => response,
-//  error => {
-//    if (error.response?.status === 401) {
-//      localStorage.removeItem('token')
-//      localStorage.removeItem('user')
-//      localStorage.removeItem('role')
-//      window.location.href = '/login'
-//    }
-//    return Promise.reject(error)
-//  }
-//)
-//
-//export default api
 
 
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:9797',
+baseURL: import.meta.env.VITE_API_URL || 'http://localhost:9797',
   headers: { 'Content-Type': 'application/json' }
 })
 
